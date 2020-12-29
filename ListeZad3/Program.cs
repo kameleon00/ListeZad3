0a7c35a943a242d02bc43eb6e161641a39268957
Marko Djokic <marko1319@its.edu.rs>
2020-12-30 00:36:33 +0100

    Add project files.

---
+++ ListeZad3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListeZad3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> godine = new List<int>() { 2009, 2010, 2011 };
+            try
+            {
+                Console.WriteLine(godine[2]);
+                Console.WriteLine(godine[5]);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
